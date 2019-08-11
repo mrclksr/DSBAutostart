a275eedc931e990dea57a4f7e762407c97d0a945
Marcel <mk@freeshell.de>
2019-08-11 23:55:37 +0200

    Update

--- locale/dsbautostart_de.ts
+++ locale/dsbautostart_de.ts
@@ -1,13 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_DE">
-<context>
-    <name>Entry</name>
-</context>
+<TS version="2.1" language="de_DE">
 <context>
     <name>List</name>
     <message>
-        <location filename="../src/list.cpp" line="39"/>
+        <location filename="../src/list.cpp" line="41"/>
         <source>Double click to edit. Use checkbox to activate/deactivate command</source>
         <translation>Doppelklick zum Editieren. Checkbox zum aktivieren/deaktivieren des Befehls</translation>
     </message>
@@ -15,59 +12,79 @@
 <context>
     <name>Mainwin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="42"/>
+        <location filename="../src/mainwin.cpp" line="49"/>
         <source>Add commands to be executed at session start</source>
         <translation>Fügen Sie Befehle hinzu, die zum Sitzungsbeginn gestartet werden sollen</translation>
     </message>
-     <message>
-        <location filename="../src/mainwin.cpp" line="48"/>
+    <message>
+        <location filename="../src/mainwin.cpp" line="57"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Rückgängig</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="65"/>
+        <source>&amp;Redo</source>
+        <translation>&amp;Wiederholen</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="72"/>
         <source>&amp;Add</source>
         <translation>&amp;Hinzufügen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="77"/>
         <source>&amp;Delete</source>
         <translation>&amp;Löschen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="56"/>
+        <location filename="../src/mainwin.cpp" line="82"/>
         <source>&amp;Up</source>
         <translation>H&amp;och</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="60"/>
+        <location filename="../src/mainwin.cpp" line="87"/>
         <source>Dow&amp;n</source>
         <translation>&amp;Runter</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="80"/>
-        <location filename="../src/mainwin.cpp" line="129"/>
+        <location filename="../src/mainwin.cpp" line="113"/>
+        <location filename="../src/mainwin.cpp" line="175"/>
         <source>&amp;Save</source>
         <translation>&amp;Speichern</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="82"/>
+        <location filename="../src/mainwin.cpp" line="115"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="123"/>
-        <location filename="../src/mainwin.cpp" line="124"/>
+        <location filename="../src/mainwin.cpp" line="145"/>
+        <source>Modified</source>
+        <translation>Verändert</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="157"/>
+        <source>Saved</source>
+        <translation>Gespeichert</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="169"/>
+        <location filename="../src/mainwin.cpp" line="170"/>
         <source>The file has been modified.</source>
         <translation>Die Datei wurde verändert.</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="125"/>
+        <location filename="../src/mainwin.cpp" line="171"/>
         <source>Do you want to save your changes?</source>
         <translation>Möchten Sie die Änderungen speichern?</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="131"/>
+        <location filename="../src/mainwin.cpp" line="177"/>
         <source>&amp;Quit without saving</source>
         <translation>&amp;Beenden ohne zu speichern</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="132"/>
+        <location filename="../src/mainwin.cpp" line="178"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
